9be580f18ee1f80c1f0b2a3d06a8c79bf2ddce33
Jimmy Rubia <rubiajimmy3@gmail.com>
2024-06-25 16:50:32 +0300

    finish login request handling

--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const connection = require('../config');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const { sign } = require('jsonwebtoken');
 
 router.post('/', (req, res) => {
     const data = req.body;
-
-    //check if username exists
+    const message = {};
+    
     const query = "SELECT username, pwd FROM users WHERE username = ?;";
     const values = [data.username];
 
@@ -14,7 +16,25 @@ router.post('/', (req, res) => {
             console.log(err);
             return res.status(500).json("Could not complete operation");
         }
-        console.log(result);
+
+        //check if username exists
+        if(result.length === 0) {
+            message.loginError = "incorrect username or password!";
+            return res.json(message);
+        }
+
+        const matchPassword = bcrypt.compare(data.password, result[0].pwd);
+
+        //check password correctness
+        if(!matchPassword) {
+            message.loginError = "incorrect username or password!";
+            return res.json(message);
+        }
+
+        //generate access token
+        const accessToken = sign({username: result[0].username}, "myUserName", {expiresIn: 1800});
+        message.loginSuccess = accessToken;
+        return res.json(message);
     })
 })
 
