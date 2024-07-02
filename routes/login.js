3aa6b73cb02fe021306cc6f51a7d169bcf287ff3
Jimmy21048 <118855884+Jimmy21048@users.noreply.github.com>
2024-07-02 10:39:04 +0300

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const connection = require('../config');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { sign } = require('jsonwebtoken');
 
 router.post('/', (req, res) => {
