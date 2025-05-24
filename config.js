d600233e1bdab0a4eeccddfb41012339e921f783
jimmy-rubia <rubiajimmy3@gmail.com>
2025-05-24 11:10:00 +0300

    change mysql charset to 'utf8mb4'

--- config.js
+++ config.js
@@ -6,7 +6,8 @@ const connection = mysql.createConnection({
     user: process.env.USER,
     database: process.env.DATABASE,
     password: process.env.PASSWORD,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    charset: 'utf8mb4'
 })
 
 module.exports = connection;
